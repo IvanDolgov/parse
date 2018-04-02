dafeabbf08f5bd0afe7aa76ed7ee13bfaa571e15
Ivan D <wanttobox@gmail.com>
2018-04-02 21:27:54 +0300

    Create obrazkras.sql

---
+++ obrazkras.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Апр 02 2018 г., 18:22
+-- Версия сервера: 5.5.57-0+deb8u1
+-- Версия PHP: 7.2.3-1+0~20180306120140.19+jessie~1.gbp81bf3b
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `obrazkras`
+--
+CREATE DATABASE IF NOT EXISTS `obrazkras123` DEFAULT CHARACTER SET koi8r COLLATE koi8r_general_ci;
+USE `obrazkras`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(255) NOT NULL,
+  `tid` int(255) NOT NULL,
+  `ser` varchar(255) DEFAULT NULL,
+  `numb` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `parseevent`
+--
+
+CREATE TABLE `parseevent` (
+  `id` int(255) NOT NULL,
+  `amount` varchar(255) DEFAULT NULL,
+  `eventtime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `parsenews`
+--
+
+CREATE TABLE `parsenews` (
+  `id` int(255) NOT NULL,
+  `id_page` int(255) NOT NULL,
+  `ser` varchar(1000) NOT NULL,
+  `numb` varchar(255) NOT NULL,
+  `datex` varchar(255) NOT NULL,
+  `page` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `tid` int(255) NOT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `regtime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `documents`
+--
+ALTER TABLE `documents`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
+
+--
+-- Индексы таблицы `parseevent`
+--
+ALTER TABLE `parseevent`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
+
+--
+-- Индексы таблицы `parsenews`
+--
+ALTER TABLE `parsenews`
+  ADD UNIQUE KEY `id_2` (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `tid` (`tid`),
+  ADD KEY `id_2` (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT для таблицы `parseevent`
+--
+ALTER TABLE `parseevent`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=405;
+
+--
+-- AUTO_INCREMENT для таблицы `parsenews`
+--
+ALTER TABLE `parsenews`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5774;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  
+  
+--
+-- Начать парсинг с 2018 года
+--
+INSERT INTO `parsenews` (`page`) VALUES ('1630'); 
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
